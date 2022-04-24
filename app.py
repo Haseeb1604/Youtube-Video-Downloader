59c68162ee2c873acf8487cb37056ba07e3431da
haseeb1604 <mhaseeb1604@gmail.com>
2022-04-24 22:09:14 +0500

    Sidebar added

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from nis import cat
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from pytube import YouTube
+from io import BytesIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
 
-@app.route('/')
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    if(request.method == 'POST'):
+    if request.method == "POST":
         session['link'] = request.form.get('url')
         try:
             url = YouTube(session['link'])
             url.check_availability()
-        except Exception as e:
-            return render_template('error.html')
-    return render_template('index.html')
+        except:
+            return render_template("error.html")
+        return render_template("youtube.html", url = url, download=True)
+    return render_template("youtube.html")
 
+@app.route("/download", methods = ["GET", "POST"])
+def download_video():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        itag = request.form.get("itag")
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name=url.title, mimetype="video/mp4")
+    return redirect(url_for("home"))
 if __name__ == '__main__':
     app.run(debug=True)
