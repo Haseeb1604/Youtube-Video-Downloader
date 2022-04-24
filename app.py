aa1f0db88840d228f8940903adbd6a09ef63fba4
haseeb1604 <mhaseeb1604@gmail.com>
2022-04-24 22:27:03 +0500

    404 Page added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
 
 @app.route("/", methods = ["GET", "POST"])
+@app.route("/youtube", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
         session['link'] = request.form.get('url')
@@ -16,6 +17,14 @@ def index():
             return render_template("error.html")
         return render_template("youtube.html", url = url, download=True)
     return render_template("youtube.html")
+
+@app.route("/facebook")
+def facebook():
+    return render_template("facebook.html")
+
+@app.route("/instagram")
+def instagram():
+    return render_template("facebook.html")
 
 @app.route("/download", methods = ["GET", "POST"])
 def download_video():
@@ -28,5 +37,10 @@ def download_video():
         buffer.seek(0)
         return send_file(buffer, as_attachment=True, download_name=url.title, mimetype="video/mp4")
     return redirect(url_for("home"))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
