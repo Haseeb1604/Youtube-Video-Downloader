196756b808e4eef2c2f95a3f3e2b011feb86e056
haseeb1604 <mhaseeb1604@gmail.com>
2022-04-25 18:22:56 +0500

    Download functionality added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from pytube import YouTube
-# from io import BytesIO
+from io import BytesIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "654c0fb3968af9d5e6a9b3edcbc7051b"
@@ -28,19 +28,19 @@ def instagram():
 
 @app.route("/download", methods = ["GET", "POST"])
 def download_video():
-#     if request.method == "POST":
-#         buffer = BytesIO()
-#         url = YouTube(session['link'])
-#         itag = request.form.get("itag")
-#         video = url.streams.get_by_itag(itag)
-#         video.stream_to_buffer(buffer)
-#         buffer.seek(0)
-#         return send_file(buffer, as_attachment=True, download_name=url.title, mimetype="video/mp4")
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        itag = request.form.get("itag")
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name=url.title, mimetype="video/mp4")
     return redirect(url_for("home"))
 
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     return render_template('page_not_found.html'), 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     app.run()
